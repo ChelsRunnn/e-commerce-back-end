19dafcfcf8d34e48b10e8dea52e0e826652f60d9
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-12-19 21:51:34 -0500

    All GET routes written

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,8 @@ router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
       // Include its associated Category and Tag data
-      include: [{ model: Product }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag }],
+      // ! category name returning as null? check insomnia
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -22,7 +23,7 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       // be sure to include its associated Products
-      include: [{ model: Category }],
+      include: [{ model: Category }, { model: Tag }],
     });
     if (!productData) {
       res.status(404).json({ message: 'No product found with that id!' });
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,15 +1,37 @@
 const router = require('express').Router();
+const { rmSync } = require('fs');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// Get all Tags
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      // Include its associated Category and Tag data
+      include: [{ model: Product }],
+      // ! Category name returning as null. check insomnia
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if(!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!'});
+      return;
+    }
+    res.status(200).json(TagData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // be sure to include its associated Product data
 });
 
